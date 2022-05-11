0489cdd9b5275774efb210869176cc40e85aab4d
Joeri Verlooy <u0140090@thomasmore.be>
2022-05-11 20:59:12 +0200

    main

---
+++ src/main/java/be/thomasmore/helloazure/HelloAzureApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.helloazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloAzureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloAzureApplication.class, args);
+    }
+
+}
